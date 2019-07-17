7603e3a178fdea626a741512d8aa57a0f7cf6319
Rostyslav Matviyiv <rmatv.g@gmail.com>
2019-07-17 22:30:01 +0300

    clean up ext_tables.php and readme

--- ext_tables.php
+++ ext_tables.php
@@ -1,20 +1,18 @@
 <?php
-if (!defined('TYPO3_MODE')) {
+
+if (! defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-$boot = function ($extensionKey='rd_contact_plugin') {
-    $extensionPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($extensionKey);
-
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-		'RaccoonDepot.RdContactPlugin',
-		'EmailActions',
-		'Email Actions'
-	);
-
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extensionKey, 'Configuration/TypoScript', 'Configuration includes (required)');
-};
-
-$boot($_EXTKEY);
-unset($boot);
-	
+(function ($extKey) {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+        'RaccoonDepot.RdContactPlugin',
+        'EmailActions',
+        'Email Actions'
+    );
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+        $extKey,
+        'Configuration/TypoScript',
+        'Configuration includes (required)'
+    );
+})('rd_contact_plugin');
