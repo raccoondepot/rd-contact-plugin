421dee10d272cdaa8c929bb356309240af898257
Rostyslav Matviyiv <rmatv.g@gmail.com>
2019-07-18 21:29:02 +0300

    [WIP] possibility to choose specific plugin to use by its uid

--- Classes/ViewHelpers/PluginViewHelper.php
+++ Classes/ViewHelpers/PluginViewHelper.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace RaccoonDepot\RdContactPlugin\ViewHelpers;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 use RaccoonDepot\RdContactPlugin\Domain\Repository\PluginRepository;
@@ -14,11 +16,12 @@ use RaccoonDepot\RdContactPlugin\Domain\Model\Plugin;
 class PluginViewHelper extends AbstractViewHelper
 {
     /**
-     * @param array $arguments
-     * @param \Closure $renderChildrenClosure
+     * @param array                     $arguments
+     * @param \Closure                  $renderChildrenClosure
      * @param RenderingContextInterface $renderingContext
      *
      * @return Plugin
+     * @throws \TYPO3\CMS\Extbase\Configuration\Exception\InvalidConfigurationTypeException
      */
     public static function renderStatic(
         array $arguments,
@@ -27,6 +30,11 @@ class PluginViewHelper extends AbstractViewHelper
     ): Plugin {
         $objectManager = GeneralUtility::makeInstance(ObjectManager::class);
         $pluginRepository = $objectManager->get(PluginRepository::class);
+
+        // get plugin settings
+        $configurationManager = $objectManager->get(ConfigurationManager::class);
+        $extbaseFrameworkConfiguration = $configurationManager->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_SETTINGS);
+        //$storagePid = $extbaseFrameworkConfiguration['plugin.']['tx_guesthouse_guesthouse.']['settings.']['storagePid'];
 
         // disable pid respect
         $querySettings = $objectManager->get(Typo3QuerySettings::class);
