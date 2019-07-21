993d70e9f938d48cac7033b28f2564363d6d6289
Rostyslav Matviyiv <rmatv.g@gmail.com>
2019-07-21 15:13:47 +0300

    now possible to set specific plugin by pluginToUseUid option

--- Classes/ViewHelpers/PluginViewHelper.php
+++ Classes/ViewHelpers/PluginViewHelper.php
@@ -4,42 +4,51 @@ declare(strict_types=1);
 namespace RaccoonDepot\RdContactPlugin\ViewHelpers;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
-use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 use RaccoonDepot\RdContactPlugin\Domain\Repository\PluginRepository;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
 use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
 use RaccoonDepot\RdContactPlugin\Domain\Model\Plugin;
+use Closure;
 
 class PluginViewHelper extends AbstractViewHelper
 {
     /**
+     * Initialize arguments
+     *
+     * @api
+     */
+    public function initializeArguments(): void
+    {
+        parent::initializeArguments();
+        $this->registerArgument('pluginToUseUid', 'integer', 'plugin which should be used');
+    }
+
+    /**
      * @param array                     $arguments
-     * @param \Closure                  $renderChildrenClosure
+     * @param Closure                   $renderChildrenClosure
      * @param RenderingContextInterface $renderingContext
      *
      * @return Plugin
-     * @throws \TYPO3\CMS\Extbase\Configuration\Exception\InvalidConfigurationTypeException
      */
     public static function renderStatic(
         array $arguments,
-        \Closure $renderChildrenClosure,
+        Closure $renderChildrenClosure,
         RenderingContextInterface $renderingContext
     ): Plugin {
         $objectManager = GeneralUtility::makeInstance(ObjectManager::class);
         $pluginRepository = $objectManager->get(PluginRepository::class);
 
-        // get plugin settings
-        $configurationManager = $objectManager->get(ConfigurationManager::class);
-        $extbaseFrameworkConfiguration = $configurationManager->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_SETTINGS);
-        //$storagePid = $extbaseFrameworkConfiguration['plugin.']['tx_guesthouse_guesthouse.']['settings.']['storagePid'];
-
         // disable pid respect
         $querySettings = $objectManager->get(Typo3QuerySettings::class);
         $querySettings->setRespectStoragePage(false);
         $pluginRepository->setDefaultQuerySettings($querySettings);
+
+        // should we use specific plugin configurations here?
+        if (! empty($arguments['pluginToUseUid'])) {
+            return $pluginRepository->findByUid((int) $arguments['pluginToUseUid']);
+        }
 
         return $pluginRepository->findAll()->getFirst();
     }
